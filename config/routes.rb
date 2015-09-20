a2d145bc68ba1634d6de0df30b94b9dab105766b
David Stinnette <dastinnette@gmail.com>
2015-09-20 16:34:08 -0600

    Add user specific resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/signup", to: "users#new"
-  resources :users, only: [:create]
   get "/profile", to: "users#show"
+  resources :users, only: [:create, :edit, :update, :show, :destroy]
+
 end
