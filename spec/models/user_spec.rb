853bc5438169f9f7087ed228e866a0858e1dddd6
David Stinnette <dastinnette@gmail.com>
2015-09-20 16:52:34 -0600

    Add user model test creates a user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  def valid_attributes
+    { name: "david",
+      email: "david@example.com",
+      password: "password", }
+  end
+
+  it "it creates a user" do
+    result = User.new(valid_attributes)
+    expect(result).to be_valid
+    expect(result.name).to eq("david")
+    expect(result.email).to eq("david@example.com")
+    expect(result.password).to eq("password")
+  end
+
 end
