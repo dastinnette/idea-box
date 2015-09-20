9bde0b59683d8229e6f5751d8c68214046ad30a0
David Stinnette <dastinnette@gmail.com>
2015-09-20 17:56:06 -0600

    Complete create user feature test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  # root :to => ""
+  root :to => "users#new"
 
   get "/signup", to: "users#new"
   get "/profile", to: "users#show"
+  get "/logout", to: "users#new"
   resources :users, only: [:create, :edit, :update, :show, :destroy]
 
 end
--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -1,20 +1,23 @@
 require 'rails_helper'
 
-feature "User signs up" do
+RSpec.describe UsersController, type: :feature do
 
-  scenario "with valid attributes" do
-    visit "/signup"
-    expect(current_path).to eq('/signup')
+  feature "User signs up" do
 
-    fill_in "Name", with: "david"
-    fill_in "Email", with: "david@example.com"
-    fill_in "Password", with: "password"
-    click_on "Sign up"
+    scenario "with valid attributes" do
+      visit "/signup"
+      expect(current_path).to eq('/signup')
 
-    expect(current_path).to eq('/profile')
+      fill_in "Name", with: "david"
+      fill_in "Email", with: "david@example.com"
+      fill_in "Password", with: "password"
+      click_on "Sign up"
 
-    within ".profile-info" do
-      expect(page).to have_content("david")
+      expect(current_path).to eq('/profile')
+
+      within ".profile-info" do
+        expect(page).to have_content("david")
+      end
     end
   end
 
