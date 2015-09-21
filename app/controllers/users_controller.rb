1e417bdbed704a6bd0d2508635debfdd41083d56
David Stinnette <dastinnette@gmail.com>
2015-09-20 18:13:16 -0600

    Add database cleaner functionality in rails_helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
 
+  def index
+    redirect_to new_user_path
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    user = User.create(user_params)
-    session[:user_id] = user.id
-    redirect_to "/profile"
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_ideas_path(@user)
+    else
+      flash[:error] = "Invalid login, please try again."
+      redirect_to new_user_path
+    end
   end
 
   def show
