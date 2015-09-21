a38c6760a5d74cafc7bfb05e9e6a4e90e067ff2e
David Stinnette <dastinnette@gmail.com>
2015-09-20 18:29:47 -0600

    Remove langauage & methods that took me away from TDD, all tests currently passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,13 @@
 class UsersController < ApplicationController
 
-  def index
-    redirect_to new_user_path
-  end
-
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to user_ideas_path(@user)
-    else
-      flash[:error] = "Invalid login, please try again."
-      redirect_to new_user_path
-    end
+    user = User.create(user_params)
+    session[:user_id] = user.id
+    redirect_to "/profile"
   end
 
   def show
