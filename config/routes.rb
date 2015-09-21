6d59e135f0171df4abe69f2fd46a5070c1ef4d66
David Stinnette <dastinnette@gmail.com>
2015-09-20 19:26:18 -0600

    Add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root :to => "users#index"
 
   get "/profile", to: "users#show"
-
+  get "/login", to: "sessions#new"
+  
   resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
 
 end
---
+++ spec/features/user_creates_an_idea_spec.rb
@@ -0,0 +1,105 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :feature do
+
+  before(:each) do
+    user = User.create(name: "david", email: "david@example.com", password: "password")
+
+    visit login_path
+    fill_in "session[name]", with: "david"
+    fill_in "session[email]", with: "david@example.com"
+    fill_in "session[password]", with: "password"
+    click_button "Login"
+  end
+
+  describe "a logged in user" do
+    it "can create an idea" do
+      user = User.find_by(name: "david")
+      visit user_ideas_path(user)
+      expect(page).to have_content("Ideas")
+
+      # within "#create_idea" do
+      #   click_on "Create"
+      # end
+      #
+      # expect(current_path).to eq(new_user_idea_path(user))
+      #
+      # fill_in "idea[name]", with: ""
+      # fill_in "idea[description]", with: "They are fun and don't shed."
+      # click_on "Create"
+      #
+      # within("#ideas") do
+      #   expect(page).to have_content("Golden Doodles")
+      # end
+    end
+  end
+  #   it "can edit an idea" do
+  #     user = User.find_by(username: "Lani")
+  #     category = Category.find_by(name: "Dogs I Like")
+  #     idea = Idea.create(name: "Golden Doodles",
+  #                        description: "They are fun and don't shed.",
+  #                        category_id: category.id,
+  #                        user_id: user.id)
+  #     visit user_ideas_path(user, idea)
+  #
+  #     within("#ideas") do
+  #       expect(page).to have_content("Golden Doodles")
+  #       click_on "Edit"
+  #     end
+  #     expect(current_path).to eq(edit_user_idea_path(user, idea))
+  #
+  #     fill_in "idea[description]", with: "They are SUPER fun and don't shed."
+  #     click_on "Submit"
+  #
+  #     within("#ideas") do
+  #       expect(page).to have_content("They are SUPER fun and don't shed.")
+  #     end
+  #   end
+  #
+  #   it "can delete an idea" do
+  #     user = User.find_by(username: "Lani")
+  #     category = Category.find_by(name: "Dogs I Like")
+  #     idea = Idea.create(name: "Golden Doodles",
+  #                        description: "They are fun and don't shed.",
+  #                        category_id: category.id,
+  #                        user_id: user.id)
+  #     visit user_ideas_path(user, idea)
+  #
+  #     within("#ideas") do
+  #       expect(page).to have_content("Golden Doodles")
+  #       click_on "Delete"
+  #     end
+  #
+  #     expect(page).not_to have_content("Golden Doodles")
+  #
+  #   end
+  #
+  # end
+  #
+  # describe "a logged in user" do
+  #   it "cant see the ideas of another logged in user" do
+  #     user = User.find_by(username: "Lani")
+  #     category = Category.find_by(name: "Dogs I Like")
+  #     idea = Idea.create(name: "Golden Doodles",
+  #                        description: "They are fun and don't shed.",
+  #                        category_id: category.id,
+  #                        user_id: user.id)
+  #     visit user_ideas_path(user, idea)
+  #
+  #     within("#ideas") do
+  #       expect(page).to have_content("Golden Doodles")
+  #     end
+  #
+  #     click_on "Logout"
+  #     User.create(username: "Travis", password: "password")
+  #     visit login_path
+  #     fill_in "session[username]", with: "Travis"
+  #     fill_in "session[password]", with: "password"
+  #     click_button "Login"
+  #     within("#ideas") do
+  #       expect(page).not_to have_content("Golden Doodles")
+  #     end
+  #   end
+  # end
+
+end
