ca3cda419cc41d654c609c18cef221fe43947487
David Stinnette <dastinnette@gmail.com>
2015-09-20 18:59:39 -0600

    Adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
 
   root :to => "users#index"
 
-  get "/signup", to: "users#new"
   get "/profile", to: "users#show"
-  get "/logout", to: "users#new"
 
   resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
 
